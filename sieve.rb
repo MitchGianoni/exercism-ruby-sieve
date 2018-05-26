b5fe8d30f01a3da6d8f5acdefdeef35baf769ece
MitchGianoni <mitchgianoni@yahoo.com>
2018-05-26 17:56:28 -0400

    all tests passed

--- sieve.rb
+++ sieve.rb
@@ -1,8 +1,40 @@
+require 'prime'
+require 'pry'
+
 class Sieve
-  def initialize(primes)
+  def initialize(limit)
+    @limit = limit
   end
 
+
   def primes
-    return [2]
+    x = []
+    Prime.each(@limit) do |prime|
+      x.push(prime)
+    end
+    x
   end
 end
+
+
+
+
+# Use the Sieve of Eratosthenes to find all the primes from 2 up to a given
+# number.
+
+# The Sieve of Eratosthenes is a simple, ancient algorithm for finding all
+# prime numbers up to any given limit. It does so by iteratively marking as
+# composite (i.e. not prime) the multiples of each prime,
+# starting with the multiples of 2.
+
+# Create your range, starting at two and continuing up to and including the given limit. (i.e. [2, limit])
+
+# The algorithm consists of repeating the following over and over:
+
+# - take the next available unmarked number in your list (it is prime)
+# - mark all the multiples of that number (they are not prime)
+
+# Repeat until you have processed each number in your range.
+
+# When the algorithm terminates, all the numbers in the list that have not
+# been marked are prime.
